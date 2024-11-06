526db5d1edb28d613b077fd5bc35b7b4c8e8c29f
Lukas Rieger <code@lukasrieger.com>
2024-11-06 07:36:42 +0100

    features

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
+#[cfg(feature = "add")]
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(feature = "sub")]
 pub fn sub(left: u64, right: u64) -> u64 {
     left - right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = sub(2, 2);
-        assert_eq!(result, 0);
-    }
-}
